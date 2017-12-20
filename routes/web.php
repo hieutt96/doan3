711a45e0c7063cabb9bb4f896a10cc785509a8c0
Hieu <hieutt.bka59@gmail.com>
2017-12-20 16:32:41 +0700

    push ngay test tren thu vien

--- routes/web.php
+++ routes/web.php
@@ -80,7 +80,7 @@ Route::group(['middleware'=>'admin'],function(){
 Route::get('guest/register/congty/hocky','Guest\RegisterController@findCongty');
 Route::get('guest/find/leader','Guest\RegisterController@findLeader');
 
-Route::group(['middleware'=>'checklecturer'],function(){
+// Route::group(['middleware'=>'checklecturer'],function(){
     Route::get('/lecturer/manage_student','Lecturer\LecturerController@manageStudent');
 
     Route::get('/lecturer/cap-nhap-thong-tin','Lecturer\LecturerController@updateInfo');
@@ -100,7 +100,7 @@ Route::group(['middleware'=>'checklecturer'],function(){
     Route::post('/lecturer/gui-thong-bao','Lecturer\LecturerController@sentNoticePost');
 
     Route::get('/lecturer/receive',['as'=>'lecturer-receive-notice','uses'=>'Lecturer\LecturerController@receviceNotice']);
-});
+// });
 Route::get('pm-home',['as'=>'pm-home','uses'=>'PM\PMController@show']);
 
 // sv
